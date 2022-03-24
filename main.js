49d5ae74ded6bb23cfcac7f38c6dbcb0c751add1
Ulrich Fladl <ulricof@gmail.com>
2022-03-24 15:17:15 +0100

    beforeVueX

--- main.js
+++ main.js
@@ -1,14 +1,64 @@
+const EventBus = new Vue();
+
+const NoteCountComponent = {
+    template:  `
+        <div>Anzahl der Notizen: <strong> {{ noteCount }}</strong></div>
+    `,
+    data() {
+        return {
+            noteCount: 0
+        }   
+    },
+    created() {
+        EventBus.$on('new-note', event => this.noteCount++ );
+    }
+}
+
 const InputComponent = {
-        template: `
+        template: ` 
                 <input 
                     type="text" 
-                    class="form-control" 
-                >`   
+                    class="form-control"
+                    :placeholder="placeholder" 
+                    v-model="note"
+                    @keyup.enter="submitNote"
+                >
+                `,
+            props: ['placeholder'],
+            data ()   {
+                return {
+                    note: ''
+                }
+            }, 
+            methods: {
+                submitNote() {
+                    EventBus.$emit("new-note", {
+                        note: this.note,
+                        timeStamp: new Date().toLocaleString()
+                    });
+                    this.note = '';
+                }
+            }
 };
 
 new Vue({
     el: '#app',
     components: {
-        "input-component": InputComponent
+        "input-component": InputComponent,
+        "note-count-compnent": NoteCountComponent
+    },
+    data: {
+        notes: [],
+        timeStamps: [],
+        placeholder: 'Gibt hir deine neu Notiz ein . . . '
+    },
+    methods: {
+        storeNote(event) {
+            this.notes.push(event.note);
+            this.timeStamps.push(event.timeStamp)
+        }
+    },
+    created() {
+        EventBus.$on('new-note', event => this.storeNote(event))
     }
 });
